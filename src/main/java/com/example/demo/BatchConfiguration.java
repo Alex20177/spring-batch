a91256fca0061baa51052dfd64b615127119dc38
Alex20177 <alejandrolopez207@gmail.com>
2023-10-28 13:08:45 -0600

    This branch use JPA to write/read row. Using JpaItemWriter and remove scheduler, spring boot start the job automatically.

--- src/main/java/com/example/demo/BatchConfiguration.java
+++ src/main/java/com/example/demo/BatchConfiguration.java
@@ -11,21 +11,10 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
-import org.springframework.batch.item.database.JdbcBatchItemWriter;
-import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
-import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
-import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import javax.sql.DataSource;
 
 @Configuration
 @EnableBatchProcessing
@@ -34,32 +23,6 @@ public class BatchConfiguration {
 
     private JobBuilderFactory jobBuilderFactory;
     private StepBuilderFactory stepBuilderFactory;
-
-    @Bean
-    public FlatFileItemReader<Employee> reader(){
-        return new FlatFileItemReaderBuilder<Employee>()
-                .name("employeeItemReader")
-                .resource(new ClassPathResource("employees.csv"))
-                .delimited()
-                .names("empCode","empName","expInYears")
-                .fieldSetMapper(new BeanWrapperFieldSetMapper<Employee>(){{setTargetType(Employee.class);}})
-                .linesToSkip(1)
-                .build();
-    }
-
-    @Bean
-    public JdbcBatchItemWriter<Profile> writer(DataSource dataSource){
-        return new JdbcBatchItemWriterBuilder<Profile>()
-                .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<Profile>())
-                .sql("INSERT INTO Profile (EMP_CODE, EMP_NAME, PROFILE_NAME) VALUES (:empCode, :empName, :profileName)")
-                .dataSource(dataSource)
-                .build();
-    }
-
-    @Bean
-    public ItemProcessor<Employee, Profile> processor(){
-        return  new EmployeeProcessor();
-    }
 
     @Bean
     public Job createEmployeeJob(EmpJobExecutionListener listener, Step step1){
@@ -73,18 +36,14 @@ public class BatchConfiguration {
     }
 
     @Bean
-    public Step step1(ItemReader<Employee> reader, ItemProcessor<Employee, Profile> processor
-    , ItemWriter<Profile> writer){
+    public Step step1(ItemReader<Employee> reader, ItemWriter<Profile> writer){
         return stepBuilderFactory.get("step1")
                 .<Employee, Profile>chunk(10)
                 .reader(reader)
-                .processor(processor)
+                .processor(new EmployeeProcessor())
                 .writer(writer)
                 .build();
     }
-    @Bean
-    public JdbcTemplate jdbcTemplate(DataSource dataSource){
-        return new JdbcTemplate(dataSource);
-    }
+
 
 }
--- src/main/java/com/example/demo/listener/EmpJobExecutionListener.java
+++ src/main/java/com/example/demo/listener/EmpJobExecutionListener.java
@@ -1,5 +1,6 @@
 package com.example.demo.listener;
 
+import com.example.demo.ProfileRepository;
 import com.example.demo.entity.Profile;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
@@ -15,8 +16,9 @@ import java.util.List;
 
 @Component
 public class EmpJobExecutionListener implements JobExecutionListener {
+
     @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private ProfileRepository profileRepository;
     @Override
     public void beforeJob(JobExecution jobExecution) {
         System.out.println("Executing jon id : "+jobExecution.getJobId());
@@ -24,16 +26,8 @@ public class EmpJobExecutionListener implements JobExecutionListener {
 
     @Override
     public void afterJob(JobExecution jobExecution) {
-        List<Profile> result = null;
-        if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
-            result = jdbcTemplate.query("SELECT EMP_CODE, EMP_NAME, PROFILE_NAME FROM profile", new RowMapper<Profile>() {
-                @Override
-                public Profile mapRow(ResultSet rs, int rowNum) throws SQLException {
-                    return new Profile(rs.getLong(1), rs.getString(2), rs.getString(3));
-                }
-            });
-        }
 
-        System.out.println("Number of records = " + result.size());
+        profileRepository.findAll().forEach(System.out::println);
+
     }
 }
